2471929ca8973fe4cc0e1ebedc5cc568fb478ff1
Darien McIntosh <darienmcintosh14@gmail.com>
2023-11-20 02:25:07 -0500

    Added updated files

--- world.js
+++ world.js
@@ -1,23 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const lookupButton = document.getElementById("lookup");
     const resultContainer = document.getElementById("result");
-    const countryInput = document.getElementById("country");
 
-    lookupButton.addEventListener("click", function() {
-        // Retrieve the value entered by the user
+    lookupButton.addEventListener("click", function () {
+        const countryInput = document.getElementById("country");
         const countryName = countryInput.value.trim();
 
         // Create an XMLHttpRequest object
         const xhr = new XMLHttpRequest();
 
-        // Adjust the query parameters based on user input
-        const queryParams = countryName !== "" ? `country=${encodeURIComponent(countryName)}` : "";
-
-        // Configure it to make a GET request to world.php with the adjusted parameters
-        xhr.open("GET", `world.php?${queryParams}`, true);
+        // Configure it to make a GET request to world.php with the country parameter
+        xhr.open("GET", `world.php?country=${encodeURIComponent(countryName)}`, true);
 
         // Set up a callback function to handle the response
-        xhr.onload = function() {
+        xhr.onload = function () {
             if (xhr.status >= 200 && xhr.status < 400) {
                 // Success! Print the response into the result container
                 resultContainer.innerHTML = xhr.responseText;
@@ -28,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
         };
 
         // Handle network errors
-        xhr.onerror = function() {
+        xhr.onerror = function () {
             resultContainer.innerHTML = "<p>Network error occurred.</p>";
         };
 
--- world.php
+++ world.php
@@ -1,28 +1,59 @@
 <?php
 $host = 'localhost';
-$username = 'lab5_user';
-$password = 'password123';
+$username = 'lab5_user'; // Replace with your actual MySQL username
+$password = 'password123'; // Replace with your actual MySQL password
 $dbname = 'world';
 
 // Retrieve the country from the GET request
 $country = isset($_GET['country']) ? $_GET['country'] : '';
 
+// Create a PDO connection
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-//$stmt = $conn->query("SELECT * FROM countries");
+// Prepare and execute the SQL query
 $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
 $stmt->bindParam(':country', $country, PDO::PARAM_STR);
 $stmt->execute();
 
+// Fetch the results
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-<?php if (!empty($results)): ?>
-  <ul>
-      <?php foreach ($results as $row): ?>
-        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-      <?php endforeach; ?>
-  </ul>
-<?php else: ?>
-  <p>No information available for the specified country.</p>
-<?php endif; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>World Database Lookup</title>
+    <link rel="stylesheet" href="world.css">
+</head>
+<body>
+
+    <h1>World Database Lookup</h1>
+
+    <?php if (!empty($results)): ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>Country Name</th>
+                    <th>Continent</th>
+                    <th>Independence Year</th>
+                    <th>Head of State</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($results as $row): ?>
+                    <tr>
+                        <td><?= $row['name']; ?></td>
+                        <td><?= $row['continent']; ?></td>
+                        <td><?= $row['independence_year']; ?></td>
+                        <td><?= $row['head_of_state']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p>No information available for the specified country.</p>
+    <?php endif; ?>
+
+</body>
+</html>
